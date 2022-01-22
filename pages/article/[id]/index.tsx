6be7e83438ae8b9a6313b99827c70256c7d4d7c8
Adeleye-jamiu <oluwaloseyifunmiadeleye1@gmail.com>
2022-01-22 11:48:50 +0100

    fix: add try and catch block to fetch method

--- pages/article/[id]/index.tsx
+++ pages/article/[id]/index.tsx
@@ -1,21 +1,21 @@
-import { server } from '../../../config'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import Meta from '../../../components/Meta'
-import { GetStaticProps, GetStaticPaths } from 'next'
+import { server } from "../../../config";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Meta from "../../../components/Meta";
+import { GetStaticProps, GetStaticPaths } from "next";
 
 export type ArticleItemType = {
-    id: string,
-    title: string,
-    excerpt: string,
-    body: string
-  }
-  
-  interface ArticleItemProps {
-    article: ArticleItemType
-  }
-   
-const article = ({article:{title, body, excerpt}}: ArticleItemProps) => {
+  id: string;
+  title: string;
+  excerpt: string;
+  body: string;
+};
+
+interface ArticleItemProps {
+  article: ArticleItemType;
+}
+
+const article = ({ article: { title, body, excerpt } }: ArticleItemProps) => {
   // const router = useRouter()
   // const { id } = router.query
 
@@ -25,56 +25,67 @@ const article = ({article:{title, body, excerpt}}: ArticleItemProps) => {
       <h1>{title}</h1>
       <p>{body}</p>
       <br />
-      <Link href='/'>Go Back</Link>
+      <Link href="/">Go Back</Link>
     </>
-  )
-}
+  );
+};
 
 export const getStaticProps: GetStaticProps = async (context: any) => {
-//   const res = await fetch(
-//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-//   )
+  //   const res = await fetch(
+  //     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  //   )
+  let article = [];
+  let error = "";
+  try {
+    const res = await fetch(`${server}/api/articles/${context.params.id}`, {
+      method: "GET",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+        Accept: "application/json; charset=UTF-8",
+      },
+    });
 
-  const res = await fetch(`${server}/api/articles/${context.params.id}`,
-  {
-    method: 'GET',
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-      Accept: 'application/json; charset=UTF-8',
-    },
-  })
-
-  const article = await res.json()
+    article = await res.json();
+  } catch (e: any) {
+    error = e.toString();
+  }
 
   return {
     props: {
       article,
+      error,
     },
-  }
-}
+  };
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-  const res = await fetch(`${server}/api/articles`,
-  {
-    method: 'GET',
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-      Accept: 'application/json; charset=UTF-8',
-    },
-  })
+  //   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  let error = "";
+  let paths = [];
+  try {
+    const res = await fetch(`${server}/api/articles`, {
+      method: "GET",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+        Accept: "application/json; charset=UTF-8",
+      },
+    });
 
-  const articles = await res.json()
+    const articles = await res.json();
 
-  const ids = articles.map((article: ArticleItemType) => article.id)
-  const paths = ids.map((id: any) => ({ params: { id: id.toString() } }))
+    const ids = articles.map((article: ArticleItemType) => article.id);
+    paths = ids.map((id: any) => ({ params: { id: id.toString() } }));
+  } catch (e: any) {
+    error = e.toString();
+  }
 
   return {
     paths,
     fallback: false,
-  }
-}
+    error
+  };
+};
 
-export default article
+export default article;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import Head from "next/head";
-import { server } from '../config'
+import { server } from "../config";
 import styles from "../styles/Home.module.css";
-import ArticleList from '../components/ArticleList'
+import ArticleList from "../components/ArticleList";
 import { ArticleItemType } from "../components/ArticleItem";
 
 interface HomeProps {
-  articles: ArticleItemType[]
+  articles: ArticleItemType[];
 }
 
-const Home = ({articles}: HomeProps): JSX.Element => {
+const Home = ({ articles }: HomeProps): JSX.Element => {
   return (
     <div className={styles.container}>
       <h1 className="ta-center">Welcome to Dev News</h1>
@@ -23,20 +22,25 @@ export const getStaticProps = async () => {
   // const res = await fetch(
   //   `https://jsonplaceholder.typicode.com/posts?_limit=6`
   // );
-  const res = await fetch(`${server}/api/articles`,
-  {
-    method: 'GET',
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-      Accept: 'application/json; charset=UTF-8',
-    },
-  })
-  const articles = await res.json();
-
+  let articles = [];
+  let error = "";
+  try {
+    const res = await fetch(`${server}/api/articles`, {
+      method: "GET",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+        Accept: "application/json; charset=UTF-8",
+      },
+    });
+    articles = await res.json();
+  } catch (e: any) {
+    error = e.toString();
+  }
   return {
     props: {
       articles,
+      error
     },
   };
 };
