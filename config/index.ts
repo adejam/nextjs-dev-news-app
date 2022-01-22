a373d7949bd3205c3dcc34a425aa96522ac90873
Adeleye-jamiu <oluwaloseyifunmiadeleye1@gmail.com>
2022-01-22 11:23:44 +0100

    fix: link still notcorrectly being deployed.

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://nextjs-dev-news-app.vercel.app/'
+export const server = dev ? 'http://localhost:3000' : 'https://nextjs-dev-news-app.vercel.app'
